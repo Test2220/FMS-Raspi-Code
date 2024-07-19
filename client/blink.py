dfd74ee332b836354d88b214e7be675aaa1543c1
Sean Christianson <62511636+SeanCh7@users.noreply.github.com>
2024-07-18 20:40:38 -0500

    Update blink.py

--- client/blink.py
+++ client/blink.py
@@ -7,5 +7,7 @@ GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 while True:
     if GPIO.input(10) == GPIO.HIGH:
         GPIO.output(8, GPIO.HIGH)
+        print("pressed")
     else:
         GPIO.output(8, GPIO.LOW)
+        print("not pressed")
