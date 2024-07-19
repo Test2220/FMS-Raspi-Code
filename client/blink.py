859719192b40534670b6829eb8ec640c94e4ff27
Sean Christianson <62511636+SeanCh7@users.noreply.github.com>
2024-07-18 20:37:14 -0500

    Update blink.py

--- client/blink.py
+++ client/blink.py
@@ -3,8 +3,8 @@ from time import sleep # Import the sleep function from the time module
 GPIO.setwarnings(False) # Ignore warning for now
 GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
 GPIO.setup(8, GPIO.OUT, initial=GPIO.LOW) # Set pin 8 to be an output pin and set initial value to low (off)
+GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 while True:
- GPIO.output(8, GPIO.HIGH)
- sleep(1)
- GPIO.output(8, GPIO.LOW)
- sleep(1)
+    if GPIO.input(10) == GPIO.HIGH:
+        GPIO.output(10, GPIO.HIGH)
+    else GPIO.output(10, GPIO.LOW)
