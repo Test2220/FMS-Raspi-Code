564ee2fddf4dff8a8dd160ca9ea1963ae124acea
Sean Christianson <62511636+SeanCh7@users.noreply.github.com>
2024-07-18 20:39:46 -0500

    Update blink.py

--- client/blink.py
+++ client/blink.py
@@ -6,6 +6,6 @@ GPIO.setup(8, GPIO.OUT, initial=GPIO.LOW) # Set pin 8 to be an output pin and se
 GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 while True:
     if GPIO.input(10) == GPIO.HIGH:
-        GPIO.output(10, GPIO.HIGH)
+        GPIO.output(8, GPIO.HIGH)
     else:
-        GPIO.output(10, GPIO.LOW)
+        GPIO.output(8, GPIO.LOW)
