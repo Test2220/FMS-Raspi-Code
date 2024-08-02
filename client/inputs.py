5b07e72fabc440ae24f6d9b9c6409dc33794ad42
Sean Christianson <62511636+SeanCh7@users.noreply.github.com>
2024-08-01 20:21:57 -0500

    k

--- client/inputs.py
+++
@@ -1,3 +0,0 @@
-from flask import Flask
-
-# get pin config from server
---
+++ client/main.py
@@ -0,0 +1,32 @@
+import os
+import RPi.GPIO
+import time
+
+GPIO.setmode(GPIO.BOARD)
+
+# setup pin 16 as an output pin
+GPIO.setup(16, GPIO.OUT, initial=GPIO.LOW)
+
+# setup pin 32 as an output pin
+GPIO.setup(32, GPIO.OUT, initial=GPIO.LOW)
+
+input_proc = os.fork()
+
+if input_proc > 0:
+    while True:
+        GPIO.output(16, GPIO.HIGH)
+        time.sleep(1)
+        GPIO.output(32, GPIO.LOW)
+        time.sleep(1)
+
+output_proc = os.fork()
+
+if output_proc > 0:
+    while True:
+        GPIO.output(16, GPIO.HIGH)
+        time.sleep(1)
+        GPIO.output(32, GPIO.LOW)
+        time.sleep(1)
+
+# wait for child processes to finish
+os.waitpid(input_proc, 0)
