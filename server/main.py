519c740f1330972b96068db772f33da0e248b0d8
Sean Christianson <62511636+SeanCh7@users.noreply.github.com>
2024-11-14 20:11:51 -0600

    Update main.py

--- server/main.py
+++ server/main.py
@@ -437,4 +437,5 @@ if game_code_pid == 0:
         sleep(0.1)
     
 if __name__ == "__main__":
-    app.run(debug=True,port=8080, host="0.0.0.0")
+    # app.run(debug=True,port=8080, host="0.0.0.0")
+    pass
